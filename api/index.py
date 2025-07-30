47b3b8ef9d5d074ebbbe9c7407242ebc6b9e4e94
boouee <104202645+boouee@users.noreply.github.com>
2025-07-30 22:50:21 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,15 +1,15 @@
 from fastapi import FastAPI, Request
 #from tgbot.main import tgbot
-from api.imap_handler import imap_handler
+from api.functions import get_teachers
 from urllib.parse import unquote, urlparse
 
 app = FastAPI()
 
 
-@app.get('/api/update')
-async def update(request: Request):
+@app.get('/api/teachers')
+async def teachers(request: Request):
     try:
-        await imap_handler()
+        await get_teachers()
     except Exception as e:
         print(e)
 
