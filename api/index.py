16cc9f738b912a018e8c6767a3977a06cad8bb09
boouee <104202645+boouee@users.noreply.github.com>
2025-08-06 19:36:45 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,15 +1,27 @@
 from fastapi import FastAPI, Request
 #from tgbot.main import tgbot
-from api.functions import get_teachers, main
+from api.functions import get_teachers, get_month_data
 from urllib.parse import unquote, urlparse
 
 app = FastAPI()
 
 
-@app.get('/api/teachers')
-async def get_handler(request: Request):
+@app.get('/api/data')
+async def get_data(request: Request, month: int):
     try:
-        await main()
+        output = await get_month_data(month)
+        return output
     except Exception as e:
         print(e)
+        return e
 
+@app.get('/api/teachers')
+async def get_data(request: Request):
+    try:
+        teachers = await get_teachers()
+        teachers = list(map(lambda teacher: teacher["name"], teachers))
+        return teachers 
+    except Exception as e:
+        print(e)
+        return e
+        
