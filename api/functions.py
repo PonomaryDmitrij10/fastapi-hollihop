2f7451b15e1f808acc5b94e88f953cd5f12f7a0c
boouee <104202645+boouee@users.noreply.github.com>
2025-08-06 21:19:19 +0500

    Update functions.py

--- api/functions.py
+++ api/functions.py
@@ -39,12 +39,13 @@ async def main():
         print(output)    
         
 async def get_month_data(month): 
-    teachers = await get_teachers()
+    
     async with httpx.AsyncClient() as client:
         dates = get_dates(month)
         date_from = dates["from"]
         date_to = dates["to"]
         output = [["", dates["title"], ""], ["Учеников", "Откол", "% откола"]]
+        teachers = await get_teachers(client)
         links = await get_all_student_unit_links(client, date_from, date_to, 0)
         students = await get_all_students(client, 0)
         for teacher in teachers:
@@ -71,8 +72,7 @@ async def get_month_data(month):
         #units = await get_units(client, 1418, "2025-01-01","2025-08-01")
         #units = list(filter(lambda unit: unit
 
-async def get_teachers():
-  async with httpx.AsyncClient() as client:
+async def get_teachers(client):
     path = api + "getteachers"
     response = await client.get(path, params=params)
     response = response.json()
