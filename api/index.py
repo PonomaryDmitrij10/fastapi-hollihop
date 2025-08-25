2e58122eb3b414fe0f0af356cdaa9fd624bc649a
boouee <104202645+boouee@users.noreply.github.com>
2025-08-25 16:13:59 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Request
 #from tgbot.main import tgbot
-from api.functions import get_teachers, get_month_data
+from api.functions import get_teachers, get_month_data, main
 from urllib.parse import unquote, urlparse
 #from pydantic import BaseMode
 import httpx
@@ -12,7 +12,7 @@ app = FastAPI()
 @app.get('/api/data')
 async def get_data(request: Request, month: int):
     try:
-        output = await get_month_data(month)
+        output = await main()
         return output
     except Exception as e:
         print(e)
