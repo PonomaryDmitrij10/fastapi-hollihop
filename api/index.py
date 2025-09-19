2408256cb5c6db4d96e658deb1e333c71c0a2d30
PonomaryDmitrij10 <ponomary.dmitrij@gmail.com>
2025-09-20 01:28:42 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from api.functions import get_month_data
+import traceback
+import sys
 
 app = FastAPI()
 
@@ -11,5 +14,13 @@ async def ping():
     return {"status": "ok"}
 
 @app.get("/api/data")
-async def get_data(month: int = 1, year: int = 2025):
-    return {"month": month, "year": year, "data": "тестовые данные"}
+async def get_data(request: Request, month: int, year: int = 2025):
+    try:
+        output = await get_month_data(month)
+        if not output or len(output) == 1:  # только заголовки, данных нет
+            return {"error": f"Нет данных за {month}/{year}"}
+        return {"data": output}
+    except Exception as e:
+        err_type, err_value, err_tb = sys.exc_info()
+        err = "".join(traceback.format_exception(err_type, err_value, err_tb))
+        return {"error": str(e), "traceback": err}
