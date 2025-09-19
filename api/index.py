8d96a72605e71d8705f06b5568d5b3699f209f3c
PonomaryDmitrij10 <ponomary.dmitrij@gmail.com>
2025-09-20 01:19:31 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,25 +1,11 @@
-from fastapi import FastAPI, Request
-from api.functions import get_month_data, get_teachers
-import traceback, sys
+from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/api/data")
-async def get_data(month: int, year: int = None):
-    try:
-        output = await get_month_data(month)
-        return output
-    except Exception as e:
-        err_type, err_value, err_tb = sys.exc_info()
-        err = "".join(traceback.format_exception(err_type, err_value, err_tb))
-        return {"error": err}
+@app.get("/")
+async def root():
+    return {"message": "🚀 FastAPI работает!"}
 
-@app.get("/api/teachers")
-async def get_teachers_list():
-    try:
-        teachers = await get_teachers(None)
-        return teachers
-    except Exception as e:
-        err_type, err_value, err_tb = sys.exc_info()
-        err = "".join(traceback.format_exception(err_type, err_value, err_tb))
-        return {"error": err}
+@app.get("/ping")
+async def ping():
+    return {"status": "ok"}
