f96c7cce0bdeeadd3064061b3fe8ed15682cabbc
PonomaryDmitrij10 <ponomary.dmitrij@gmail.com>
2025-09-20 01:26:22 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,39 +1,15 @@
-from fastapi import FastAPI, Request
-from api.functions import get_month_data, get_teachers
-import traceback, sys, httpx
+from fastapi import FastAPI
 
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
     return {"message": "🚀 FastAPI работает!"}
 
-
 @app.get("/ping")
 async def ping():
     return {"status": "ok"}
 
-
 @app.get("/api/data")
-async def get_data(month: int, year: int):
-    try:
-        output = await get_month_data(month, year)
-        return output
-    except Exception as e:
-        err_type, err_value, err_tb = sys.exc_info()
-        err = "".join(traceback.format_exception(err_type, err_value, err_tb))
-        return {"error": str(e), "trace": err}
-
-
-@app.get("/api/teachers")
-async def get_teachers_list(request: Request):
-    try:
-        async with httpx.AsyncClient() as client:
-            teachers = await get_teachers(client)
-            teachers = [t["name"] for t in teachers]
-            return teachers
-    except Exception as e:
-        err_type, err_value, err_tb = sys.exc_info()
-        err = "".join(traceback.format_exception(err_type, err_value, err_tb))
-        return {"error": str(e), "trace": err}
+async def get_data(month: int = 1, year: int = 2025):
+    return {"month": month, "year": year, "data": "тестовые данные"}
