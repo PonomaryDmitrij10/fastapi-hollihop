f6ec66e8e5a388696638852f0374fc833474e7e3
PonomaryDmitrij10 <ponomary.dmitrij@gmail.com>
2025-09-20 01:04:02 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,20 +1,31 @@
 from fastapi import FastAPI, Request
-from api.functions import get_month_data
+from api.functions import get_teachers, get_month_data
+import httpx
 import traceback
 import sys
 
 app = FastAPI()
 
-# ===== Эндпоинт статистики =====
 @app.get("/api/data")
 async def get_data(request: Request, month: int, year: int):
     try:
-        print(f"📅 Запрос данных за {month}/{year}")
-        output = await get_month_data(month, year)
-        print("✅ Успешно получили данные")
+        print(f"📅 Запрос данных за месяц: {month}, год: {year}")
+        output = await get_month_data(month)
         return output
     except Exception as e:
         err_type, err_value, err_tb = sys.exc_info()
         err = "".join(traceback.format_exception(err_type, err_value, err_tb))
-        print("❌ Полная ошибка в get_data:\n", err)
+        return {"error": err}
+
+
+@app.get("/api/teachers")
+async def get_teachers_list(request: Request):
+    try:
+        async with httpx.AsyncClient() as client:
+            teachers = await get_teachers(client)
+            teachers = [teacher["name"] for teacher in teachers]
+            return teachers
+    except Exception as e:
+        err_type, err_value, err_tb = sys.exc_info()
+        err = "".join(traceback.format_exception(err_type, err_value, err_tb))
         return {"error": err}
